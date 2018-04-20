91b6ae7b233377655ec71a94848b21193030456b
Cherry <hanhchuyen6791@yahoo.com>
2018-04-20 22:58:40 +0700

    Setting Up Project Folders

---
+++ breakpoint/Controller/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  breakpoint
+//
+//  Created by mac on 4/20/18.
+//  Copyright © 2018 DoThuyHanhChuyen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
