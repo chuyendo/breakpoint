4c023085748178031dd6cbc7bf92625ba1023f0f
Cherry <hanhchuyen6791@yahoo.com>
2018-04-30 18:13:26 +0700

    Creating UserCell

---
+++ breakpoint/View/UserCell.swift
@@ -0,0 +1,33 @@
+//
+//  UserCell.swift
+//  breakpoint
+//
+//  Created by mac on 4/30/18.
+//  Copyright © 2018 DoThuyHanhChuyen. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var checkImage: UIImageView!
+    
+    func configureCell(profileImage image: UIImage, email: String, isSelected: Bool) {
+        self.profileImage.image = image
+        self.emailLbl.text = email
+        if isSelected {
+            checkImage.isHidden = false
+        } else {
+            checkImage.isHidden = true
+        }
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
