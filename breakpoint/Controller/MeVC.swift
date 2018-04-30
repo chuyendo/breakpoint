bc1d0e91b046e75bc0b727485e10b1c5cde8c7e8
Cherry <hanhchuyen6791@yahoo.com>
2018-04-30 16:25:10 +0700

    Converting UIDs into Emails and Reversing the Order of a TableView

--- breakpoint/Controller/MeVC.swift
+++ breakpoint/Controller/MeVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class MeVC: UIViewController {
 
@@ -18,6 +19,23 @@ class MeVC: UIViewController {
         super.viewDidLoad()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.emailLbl.text = Auth.auth().currentUser?.email
+    }
+    
     @IBAction func signOutBtnWasPressed(_ sender: Any) {
+        let logoutPopup = UIAlertController(title: "Logout?", message: "Do you want logout?", preferredStyle: .actionSheet)
+        let logoutAction = UIAlertAction(title: "logout?", style: .destructive) { (buttonTapped) in
+            do {
+                try Auth.auth().signOut()
+                let authVC = self.storyboard?.instantiateViewController(withIdentifier: "AuthVC") as? AuthVC
+                self.present(authVC!, animated: true, completion: nil)
+            } catch {
+                print(error)
+            }
+        }
+        logoutPopup.addAction(logoutAction)
+        present(logoutPopup, animated: true, completion: nil)
     }
 }
